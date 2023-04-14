89b0e27aa0c5cb224b14dd4dc6c3419f798ca9f3
kaushal1999 <kaushalwaghela1999@gmail.com>
2023-04-14 13:33:23 +0530

    completed the project

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-let button = document.querySelector(".btn");
 let text = document.querySelector("#box");
 let list = document.querySelector("#list_item");
 
-function del()  {
+function markAsDone()  {
     this.classList.add("deleted");
     this.removeEventListener("click", del);
+    let checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.checked = "true";
+    checkBox.style.width="20%"
+    this.prepend(checkBox);
+    this.removeEventListener("click", markAsDone);
     return;
 }
 
+function del() {
+    this.parentNode.remove();
+}
 
 let add_item = () => {
     if (text.value == "") {
         return;
     }
     let item = document.createElement("li");
-    item.innerText = text.value;
+    let span = document.createElement("span");
+    span.innerText = text.value;
+    span.style.color = "black";
+    span.style.width = "100%";
+    let button = document.createElement("button");
+    button.innerHTML = "X";
+    item.appendChild(span);
+    item.appendChild(button);
     list.appendChild(item);
-    item.addEventListener("click", del);
+    span.addEventListener("click", markAsDone);
+    button.addEventListener("click", del);
 }
+
